77c7d5059287412f56e529166fbf3e0e29b2be54
王兵 <pithyone@vip.qq.com>
2018-05-18 18:07:15 +0800

    回答模型新增link

--- src/Extractors/Answer/Answers.php
+++ src/Extractors/Answer/Answers.php
@@ -10,6 +10,7 @@ class Answers implements Extractor
     {
         return $crawler->filter('div[tabindex="-1"]')->each(function ($node) {
             return [
+                'link'        => $node->filter('link')->attr('href'),
                 'author'      => Author::extract($node),
                 'author_link' => AuthorLink::extract($node),
                 'bio'         => Bio::extract($node),
