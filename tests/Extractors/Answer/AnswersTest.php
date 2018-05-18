045e4e39fef2babc6e144ee5fe7f7d7f277da84d
王兵 <pithyone@vip.qq.com>
2018-05-18 21:15:14 +0800

    修改回答模型回答列表提取器测试

--- tests/Extractors/Answer/AnswersTest.php
+++ tests/Extractors/Answer/AnswersTest.php
@@ -20,12 +20,11 @@ class AnswersTest extends TestCase
     }
 
     /**
+     * @dataProvider exceptionProvider
      * @expectedException \InvalidArgumentException
      */
-    public function testException()
+    public function testException($html)
     {
-        $html = '<div tabindex="-1"></div>';
-
         Answers::extract(new Crawler($html));
     }
 
@@ -39,6 +38,7 @@ class AnswersTest extends TestCase
         $this->assertInternalType('array', $answers);
 
         foreach ($answers as $answer) {
+            $this->assertArrayHasKey('link', $answer);
             $this->assertArrayHasKey('author', $answer);
             $this->assertArrayHasKey('author_link', $answer);
             $this->assertArrayHasKey('bio', $answer);
@@ -47,11 +47,22 @@ class AnswersTest extends TestCase
         }
     }
 
+    public function exceptionProvider()
+    {
+        return [
+            ['<div tabindex="-1"></div>'],
+            ['<div tabindex="-1"><link></div>'],
+            ['<div tabindex="-1"><div class="zm-item-vote-info"></div></div>'],
+        ];
+    }
+
     public function extractProvider()
     {
         return [
-            ['<div tabindex="-1"><div class="zm-item-vote-info"></div></div>'],
-            ['<div tabindex="-1"><div class="zm-item-vote-info" data-votecount="1234"></div></div>'],
+            ['<div tabindex="-1"><link><div class="zm-item-vote-info"></div></div>'],
+            ['<div tabindex="-1"><link href=""><div class="zm-item-vote-info"></div></div>'],
+            ['<div tabindex="-1"><link><div class="zm-item-vote-info" data-votecount=""></div></div>'],
+            ['<div tabindex="-1"><link href=""><div class="zm-item-vote-info" data-votecount=""></div></div>'],
         ];
     }
 }
